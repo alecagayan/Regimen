9f31b88faba788a1ceafc8804ccaa8448e1ace38
Alec <alecagayan24@gmail.com>
2026-08-28 01:43:32 -0400

    Initial Commit

---
+++ Regimen/RegimenApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegimenApp.swift
+//  Regimen
+//
+//  Created by Alec Agayan on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RegimenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RegimenTests/RegimenTests.swift
@@ -0,0 +1,19 @@
+//
+//  RegimenTests.swift
+//  RegimenTests
+//
+//  Created by Alec Agayan on 8/28/26.
+//
+
+import Testing
+@testable import Regimen
+
+struct RegimenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
